57fce4fc9a8b5b4cecce0b9fa0ba4e4d40079156
hiroki shimizu <hiroki71027923@icloud.com>
2024-11-14 12:34:03 +0900

    railsでタスク管理アプリ作成完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,56 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy, :togle]
+  
+  def index 
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path, status: :see_other
+    
+  end
+
+  def togle
+    @task.update(completed: !@task.completed)
+    render turbo_stream: turbo_stream.replace(
+      @task,
+      partial: 'completed',
+      locals: {task: @task}
+    )
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
